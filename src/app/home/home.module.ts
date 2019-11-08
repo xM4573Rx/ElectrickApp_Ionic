8e3b8aaae9bfe065120fefd6d65ba7d60ea0d272
JORGE <jorgealopez@unicesar.edu.co>
2019-11-08 17:52:05 -0500

    1.0.0

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    NgCircleProgressModule.forRoot(),
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ])
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
