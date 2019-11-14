94bb70f35bb1abbb6caa7005f5e3f0a622904a5d
JORGE <jorgealopez@unicesar.edu.co>
2019-11-13 21:21:56 -0500

    1.0.4

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,14 +11,38 @@ import { TimerPage } from '../timer/timer.page';
 })
 export class HomePage {
 
-  items = [];
-  ref = firebase.database().ref('users/+573016683176/');
+  users: string = 'users/+573016683176/';
+
+  items: string = '';
+  Aenergy: string = '';
+  Apower: string = '';
+  Denergy = [];
+  Cenergy: number = 0.0;
+  Tcenergy: string = '';
+  refe = firebase.database().ref(this.users);
   inputText: string = '';
+  offDate: any = '';
 
   constructor(public navCtrl: NavController) {
-    this.ref.on('value', resp => {
-      this.items = snapshotToArray(resp);
+
+    this.refe.on('value', snap => {
+      // this.items = snapshotToArray(snap);
+      this.Aenergy = snap.child('All').val().Energy + ' kWh';
+      this.Apower = snap.child('All').val().Power + ' kWh';
+      this.Denergy = snap.child('Device').val().Energy;
+      console.log(this.Denergy);
+      this.Cenergy = (parseFloat(this.Aenergy) * 422.3);
+      this.Tcenergy = this.Cenergy.toFixed(2).toString() + ' $';
+      // console.log(variable);
+      /*this.users = this.users + variable + '/';
+      this.refe = firebase.database().ref(this.users);
+      this.refe.on('child_changed', snap2 => {
+        const variable2 = snap2.ref;
+        console.log(variable2.toString());
+      });*/
+      // console.log(this.ref);
     });
+    console.log(this.offDate);
   }
 
   openTimerPage(item) {
@@ -27,8 +51,7 @@ export class HomePage {
 
   addItem(item) {
     if (item !== undefined && item != null) {
-      let newItem = this.ref.push();
-      newItem.set(item);
+      this.refe.child('All').child('Energy').push().set(item);
       this.inputText = '';
     }
   }
--- src/app/timer/timer.page.ts
+++ src/app/timer/timer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-timer',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerPage implements OnInit {
 
-  constructor() { }
+  users: string = 'users/+573016683176/';
 
-  ngOnInit() {
+  onDate: any = '';
+  offDate: any = '';
+  refe = firebase.database().ref(this.users);
+
+  constructor() {
+
+    if (this.offDate !== undefined && this.offDate != null) {
+      this.refe.child('Device').child('Off').push().set({Off: this.offDate});
+    }
+    console.log(this.offDate);
   }
 
+  ngOnInit() {
+    console.log(this.offDate);
+  }
+
+  addItem(item) {
+    if (item !== undefined && item != null) {
+      this.refe.child('All').child('Energy').set(item);
+    }
+  }
 }
